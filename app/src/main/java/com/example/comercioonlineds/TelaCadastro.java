86e5e5b8a2894e9b7d831b1f329efbe393550a7c
JV <joao.v.cunha6@ba.estudante.senai.br>
2024-06-06 01:47:06 -0300

    Firebase, Cadastro e Login

--- app/src/main/java/com/example/comercioonlineds/TelaCadastro.java
+++ app/src/main/java/com/example/comercioonlineds/TelaCadastro.java
@@ -1,24 +1,82 @@
 package com.example.comercioonlineds;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 
 public class TelaCadastro extends AppCompatActivity {
+
+    private EditText email_cadastro;
+    private EditText senha_cadastro;
+    private EditText confirmarsenha_cadastro;
+    private Button cadastrar_btn_cadastro;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tela_cadastro);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        email_cadastro = findViewById(R.id.email_cadastro);
+        senha_cadastro = findViewById(R.id.senha_cadastro);
+        confirmarsenha_cadastro = findViewById(R.id.confirmarsenha_cadastro);
+        cadastrar_btn_cadastro = findViewById(R.id.cadastrar_btn_cadastro);
+        email_cadastro = findViewById(R.id.email_cadastro);
+
+        cadastrar_btn_cadastro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String email = email_cadastro.getText().toString();
+                String senha = senha_cadastro.getText().toString();
+                String confirmarsenha = confirmarsenha_cadastro.getText().toString();
+
+                if(!TextUtils.isEmpty(email) || !TextUtils.isEmpty(senha) || !TextUtils.isEmpty(confirmarsenha)){
+                    if(senha.equals(confirmarsenha)){
+
+                        mAuth.createUserWithEmailAndPassword(email,senha)
+                                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                                    @Override
+                                    public void onComplete(@NonNull Task<AuthResult> task) {
+
+                                        if(task.isSuccessful()){
+                                            telaprodutos();
+                                        }
+                                        else {
+                                            String error = task.getException().getMessage();
+                                            Toast.makeText(TelaCadastro.this, "" + error, Toast.LENGTH_SHORT).show();
+                                        }
+
+                                    }
+                                });
+                    }else {
+                        Toast.makeText(TelaCadastro.this, "A senha deve ser a mesma em ambos os campos!", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+        });
     }
 
-    public void telalogin (View view) {
+    public void telaprodutos () {
 
-        Intent in = new Intent(TelaCadastro.this, TelaLogin.class);
+        Intent in = new Intent(TelaCadastro.this, TelaProdutosServicos.class);
         startActivity(in);
+        finish();
 
     }
 }
--- app/src/main/java/com/example/comercioonlineds/TelaLogin.java
+++ app/src/main/java/com/example/comercioonlineds/TelaLogin.java
@@ -1,23 +1,76 @@
 package com.example.comercioonlineds;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.Firebase;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class TelaLogin extends AppCompatActivity {
+
+    private EditText email;
+    private EditText senha;
+    private Button login_btn;
+    private Button cadastro_btn;
+    private Button recuperarsenha_btn;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tela_login);
+
+        mAuth = FirebaseAuth.getInstance();
+        email = findViewById (R.id.email);
+        senha = findViewById (R.id.senha);
+        login_btn = findViewById (R.id.login_btn);
+        cadastro_btn = findViewById (R.id.cadastro_btn);
+        recuperarsenha_btn = findViewById (R.id.recuperarsenha_btn);
+
+        login_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                String loginEmail = email.getText().toString();
+                String loginSenha = senha.getText().toString();
+
+                if (!TextUtils.isEmpty(loginEmail) || !TextUtils.isEmpty(loginSenha));
+                mAuth.signInWithEmailAndPassword(loginEmail, loginSenha)
+                        .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+
+                                if(task.isSuccessful()){
+                                    telaprodutos();
+                                }
+                                else{
+                                    String error = task.getException().getMessage();
+                                    Toast.makeText(TelaLogin.this, "Email ou senha incorreto! " + error, Toast.LENGTH_SHORT).show();
+                                }
+
+                            }
+                        });
+
+            }
+        });
     }
 
-    public void telaprodutos (View view) {
+    public void telaprodutos () {
 
         Intent in = new Intent(TelaLogin.this, TelaProdutosServicos.class);
         startActivity(in);
+        finish();
 
     }
 
